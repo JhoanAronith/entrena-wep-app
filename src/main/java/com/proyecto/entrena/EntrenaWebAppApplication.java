04f6d2726f581ee9932a1024a6d87cc317397906
JhoanAronith <jhoanmunozman@gmail.com>
2025-06-02 11:05:09 -0500

    Commit inicial, estrucutura del proyecto 02/06/2025

---
+++ src/main/java/com/proyecto/entrena/EntrenaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.entrena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrenaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrenaWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/entrena/EntrenaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.entrena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
