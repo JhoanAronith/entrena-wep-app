2d5722cd6aac3aee45aa1dc79a83eb68d560b28e
alenlenlen2 <alenlen2802@gmail.com>
2025-06-06 00:18:19 -0500

    Delete EjercicioRepository.java

--- EjercicioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.entrena.repository;
-
-import com.proyecto.entrena.modelo.Ejercicio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EjercicioRepository extends JpaRepository <Ejercicio, Long>{
-}
